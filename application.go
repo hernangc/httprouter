8e8514708c4fe0615bace3dbef89294ea0403c98
Hernan Gonzalez Correa <hernangonzzalez@gmail.com>
2026-05-12 22:43:35 -0300

    feature: refactored general usage of http.HandlerFunc concrete implementation in benefit of http.Handler interface for scalability

--- application.go
+++ application.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 )
 
-type methodHandlerMap map[string]http.HandlerFunc
+type methodHandlerMap map[string]http.Handler
 
 type Application struct {
 	mux                  *http.ServeMux
@@ -24,22 +24,22 @@ func (a *Application) WithGlobalMiddlewares(mws ...Middleware) WebApplication {
 	return a
 }
 
-func (a *Application) handle(method string, path string, handler http.HandlerFunc, mws ...Middleware) {
+func (a *Application) handle(method string, path string, handler http.Handler, mws ...Middleware) {
 	if a.pathMethodHandlerMap[path] == nil {
 		a.pathMethodHandlerMap[path] = make(methodHandlerMap)
+
+		a.pathMethodHandlerMap[path][method] = a.middleware(handler, mws...)
 
 		a.mux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
 			pMH := a.pathMethodHandlerMap[path]
 			if h, ok := pMH[r.Method]; ok {
-				h(w, r)
+				h.ServeHTTP(w, r)
 				return
 			}
 			w.Header().Set("Allow", a.allowedMethods(path))
 			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		})
 	}
-
-	a.pathMethodHandlerMap[path][method] = a.middleware(handler, mws...)
 }
 
 func (a *Application) allowedMethods(path string) string {
@@ -53,36 +53,36 @@ func (a *Application) allowedMethods(path string) string {
 	return methods
 }
 
-func (a *Application) middleware(handler http.HandlerFunc, mws ...Middleware) http.HandlerFunc {
+func (a *Application) middleware(handler http.Handler, mws ...Middleware) http.Handler {
 	// First, apply route-specific middleware
 	result := a.applyMiddleware(handler, mws...)
 	// Then apply global middleware
 	return a.applyMiddleware(result, a.mws...)
 }
 
-func (a *Application) applyMiddleware(handler http.HandlerFunc, mws ...Middleware) http.HandlerFunc {
+func (a *Application) applyMiddleware(handler http.Handler, mws ...Middleware) http.Handler {
 	for i := len(mws) - 1; i >= 0; i-- {
 		handler = mws[i](handler)
 	}
 	return handler
 }
 
-func (a *Application) Post(path string, handler http.HandlerFunc, mws ...Middleware) {
+func (a *Application) Post(path string, handler http.Handler, mws ...Middleware) {
 	a.handle(http.MethodPost, path, handler, mws...)
 }
 
-func (a *Application) Get(path string, handler http.HandlerFunc, mws ...Middleware) {
+func (a *Application) Get(path string, handler http.Handler, mws ...Middleware) {
 	a.handle(http.MethodGet, path, handler, mws...)
 }
 
-func (a *Application) Put(path string, handler http.HandlerFunc, mws ...Middleware) {
+func (a *Application) Put(path string, handler http.Handler, mws ...Middleware) {
 	a.handle(http.MethodPut, path, handler, mws...)
 }
 
-func (a *Application) Patch(path string, handler http.HandlerFunc, mws ...Middleware) {
+func (a *Application) Patch(path string, handler http.Handler, mws ...Middleware) {
 	a.handle(http.MethodPatch, path, handler, mws...)
 }
 
-func (a *Application) Delete(path string, handler http.HandlerFunc, mws ...Middleware) {
+func (a *Application) Delete(path string, handler http.Handler, mws ...Middleware) {
 	a.handle(http.MethodDelete, path, handler, mws...)
 }
